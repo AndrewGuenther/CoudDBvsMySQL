2d7f947d393379614cb2cb768741df2260fe1dd5
Marc Zych <marczych@gmail.com>
2012-11-18 08:29:18 +0000

    Fix ordering of CREATE TABLE statements

--- schema.sql
+++ schema.sql
@@ -1,4 +1,8 @@
+DROP TABLE IF EXISTS education;
+DROP TABLE IF EXISTS people_addresses;
+DROP TABLE IF EXISTS addresses;
 DROP TABLE IF EXISTS people;
+
 CREATE TABLE people (
    personid int(11) NOT NULL AUTO_INCREMENT,
    surname varchar(128),
@@ -8,17 +12,6 @@ CREATE TABLE people (
    PRIMARY KEY (personid)
 );
 
-DROP TABLE IF EXISTS people_addresses;
-CREATE TABLE people_addresses (
-   personid int(11) NOT NULL,
-   addressid int(11) NOT NULL,
-   isPrimary tinyint(1),
-   PRIMARY KEY (personid, addressid),
-   FOREIGN KEY (personid) REFERENCES people(personid) ON DELETE CASCADE,
-   FOREIGN KEY (addressid) REFERENCES addresses(addressid) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS addresses;
 CREATE TABLE addresses (
    addressid int(11) NOT NULL AUTO_INCREMENT,
    line1 varchar(128),
@@ -30,7 +23,15 @@ CREATE TABLE addresses (
    PRIMARY KEY (addressid)
 ) ENGINE=INNODB;
 
-DROP TABLE IF EXISTS education;
+CREATE TABLE people_addresses (
+   personid int(11) NOT NULL,
+   addressid int(11) NOT NULL,
+   isPrimary tinyint(1),
+   PRIMARY KEY (personid, addressid),
+   FOREIGN KEY (personid) REFERENCES people(personid) ON DELETE CASCADE,
+   FOREIGN KEY (addressid) REFERENCES addresses(addressid) ON DELETE CASCADE
+);
+
 CREATE TABLE education (
    personid int(11) NOT NULL,
    institution varchar(256),
