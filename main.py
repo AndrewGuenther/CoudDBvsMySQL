49cc10a160b8315929482ad095976b55d8693ef0
Marc Zych <marczych@gmail.com>
2012-11-19 02:28:29 +0000

    Start with a fresh schema

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from db.couchdb import CouchDB
 mydb = MySqlDb()
 couchdb = CouchDB()
 
+# Drop all tables and start from scratch.
+schema = open('schema.sql').read()
+schemaStatements = schema.split(';\n')
+for statement in schemaStatements:
+   if statement:
+      mydb.executeWrite(statement)
+
 #person = PersonBuffer.getNewPerson()
 # couchdb.insertPerson(person)
 
@@ -15,7 +22,7 @@ couchdb = CouchDB()
 #print mydb.executeWrite("insert into testz set id = %s, value = %s", (1337, 'dsfwef'))
 #print mydb.get('dfwf')
 
-for _ in range(1, 10):
+for _ in range(1, 1000):
    mydb.insertPerson(PersonBuffer.getNewPerson())
 
 pprint(mydb.getPerson(10))
