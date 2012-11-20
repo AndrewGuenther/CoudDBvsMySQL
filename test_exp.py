4142159fad50358dc8aa0a99894dd7c63ba490a9
Marc Zych <marczych@gmail.com>
2012-11-20 03:54:32 +0000

    Add comment to test_exp

--- test_exp.py
+++ test_exp.py
@@ -2,6 +2,8 @@ from experiment import Experiment, DBType
 from util.stats import Stats
 
 class TestExperiment(Experiment):
+   """Simple test experiment that performs some writes."""
+
    def main(self):
       self.insertPeople(10)
       Stats.output()
