47d04091b40f6ea54f7d231c0bab804a5b813ad6
Marc Zych <marczych@gmail.com>
2012-11-20 07:50:59 +0000

    Make read experiment only do simple reads

--- exp_read.py
+++ exp_read.py
@@ -5,8 +5,7 @@ class ReadExperiment(Experiment):
    """Simple experiment that performs some reads."""
 
    def main(self):
-      self.getPeople(5000)
-      self.getPeopleAndParents(5000)
+      self.getPeople(10000)
       Stats.output()
 
 exp = ReadExperiment()
