5d698f77027d081f41232f48a3064138f5f89f2c
Marc Zych <marczych@gmail.com>
2012-11-20 06:12:42 +0000

    Clean up some experiments

--- exp_write.py
+++ exp_write.py
@@ -1,13 +1,12 @@
 from experiment import Experiment, DBType
 from util.stats import Stats
 
-class TestExperiment(Experiment):
-   """Simple test experiment that performs some writes."""
+class WriteExperiment(Experiment):
+   """Simple experiment that performs some writes."""
 
    def main(self):
       self.insertPeople(10000)
       Stats.output()
-      print "done"
 
-exp = TestExperiment()
+exp = WriteExperiment()
 exp.main()
